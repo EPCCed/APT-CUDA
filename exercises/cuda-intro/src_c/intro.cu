ce8a43df05e07ba0d5348551f20229f4422fc068
Rupert Nash <r.nash@epcc.ed.ac.uk>
2024-01-29 15:44:19 +0000

    Solution for intro exercise

--- exercises/cuda-intro/src_c/intro.cu
+++ exercises/cuda-intro/src_c/intro.cu
@@ -30,12 +30,16 @@ void checkCUDAError(const char*);
 __global__ void negate(int *d_a)
 {
     /* Part 2B: negate an element of d_a */
+  int idx = threadIdx.x;
+  d_a[idx] = -1 * d_a[idx];
 }
 
 /* Multi-block version of kernel for part 2C */
 __global__ void negate_multiblock(int *d_a)
 {
     /* Part 2C: negate an element of d_a, using multiple blocks this time */
+  int idx = threadIdx.x + (blockIdx.x * blockDim.x);
+  d_a[idx] = -1 * d_a[idx];
 }
 
 /* Main routine */
@@ -68,6 +72,7 @@ int main(int argc, char *argv[])
      * allocate memory on device
      */
     /* Part 1A: allocate device memory */
+    cudaMalloc(&d_a, sz);
 
     /* initialise host arrays */
     for (i = 0; i < ARRAY_SIZE; i++) {
@@ -77,12 +82,17 @@ int main(int argc, char *argv[])
 
     /* copy input array from host to GPU */
     /* Part 1B: copy host array h_a to device array d_a */
+    cudaMemcpy(d_a, h_a, sz, cudaMemcpyHostToDevice);
 
     /* run the kernel on the GPU */
     /* Part 2A: configure and launch kernel (un-comment and complete) */
     /* dim3 blocksPerGrid( ); */
     /* dim3 threadsPerBlock( ); */
     /* negate<<< , >>>( ); */
+    dim3 blocksPerGrid(NUM_BLOCKS,1,1);
+    dim3 threadsPerBlock(THREADS_PER_BLOCK,1,1);
+    negate<<< blocksPerGrid, threadsPerBlock >>>(d_a);
+    /*negate_multiblock<<< blocksPerGrid, threadsPerBlock >>>(d_a); */ 
 
     /* wait for all threads to complete and check for errors */
     cudaDeviceSynchronize();
@@ -90,6 +100,7 @@ int main(int argc, char *argv[])
 
     /* copy the result array back to the host */
     /* Part 1C: copy device array d_a to host array h_out */
+    cudaMemcpy(h_out, d_a, sz, cudaMemcpyDeviceToHost);
 
     checkCUDAError("memcpy");
 
@@ -102,6 +113,7 @@ int main(int argc, char *argv[])
 
     /* free device buffer */
     /* Part 1D: free d_a */
+    cudaFree(d_a);
 
     /* free host buffers */
     free(h_a);
